5440fbf9bab5e7cb0fc06b5d2ec931376ff029a2
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-05-26 15:48:49 +0900

    간단한 테스트 추가

---
+++ src/test/java/com/example/demo/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @DisplayName("Simple Test")
+    @Test
+    void simpleTest() {
+        assert true;
+    }
+}
