135747fb47c3152007fe898964b544a483401be0
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-05-28 17:39:43 +0900

    소켓 서버 좀 더 깔끔하게 수정

--- src/main/java/app/controller/SocketClientController.java
+++ src/main/java/app/controller/SocketClientController.java
@@ -5,10 +5,8 @@ import app.service.SocketClientService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -30,17 +28,14 @@ public class SocketClientController {
     public String getMyInfo() {
         return socketClientService.getMyInfo();
     }
+
     @PostMapping("/client")
-    public String  addDVM(Info info) throws JsonProcessingException {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<Object> addDVM(@RequestBody Info info) {
         try {
-            Info result =  socketClientService.addInfo(info);
-            response.put("status", "success");
-            response.put("data", result);
+            Info result = socketClientService.addInfo(info);
+            return ResponseEntity.ok(result);
         } catch (Exception e) {
-            response.put("status", "error");
-            response.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-        return objectMapper.writeValueAsString(response);
     }
 }
