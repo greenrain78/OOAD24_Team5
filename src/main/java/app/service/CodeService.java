4579075e51f798276bca6d8420338d615c46637b
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-05-30 10:24:45 +0900

    timer 구성

---
+++ src/main/java/app/service/CodeService.java
@@ -0,0 +1,45 @@
+package app.service;
+
+import app.domain.Code;
+import app.domain.Item;
+import app.repository.CodeRepository;
+import app.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CodeService {
+    @Autowired
+    private CodeRepository codeRepository;
+    public List<Code> getAllCodes() {
+        return codeRepository.findAll();
+    }
+    public Optional<Code> getCodeByCode(String code) {
+        return Optional.ofNullable(codeRepository.findByCode(code));
+    }
+    public Code createCode(Code code) {
+        return codeRepository.save(code);
+    }
+    @Transactional
+    public Code deleteCode(String code) {
+        Code codeToRemove = codeRepository.findByCode(code);
+        codeRepository.delete(codeToRemove);
+        return codeToRemove;
+    }
+    // 기간이 지난 코드는 삭제
+    @Transactional
+    public void deleteExpiredCodes() {
+        int ExpiredDays = 30;
+        List<Code> codes = codeRepository.findAll();
+        for (Code code : codes) {
+            // 오늘 날짜 기준으로 30일 이전 코드 삭제
+            if (code.getTime().plusDays(ExpiredDays).isBefore(code.getTime())) {
+                codeRepository.delete(code);
+            }
+        }
+    }
+}
---
+++ src/main/java/app/service/Timer.java
@@ -0,0 +1,14 @@
+package app.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+@Slf4j
+@Component
+public class Timer {
+    // 고정된 주기로 실행 (밀리초 단위)
+    @Scheduled(fixedRate = 1000)
+    public void fixedRateTask() {
+        log.info("Fixed rate task - " + System.currentTimeMillis() / 1000);
+    }
+}
