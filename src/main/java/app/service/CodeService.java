696b4009182005ac0a6e9b1d50efc23f236d9463
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-05-30 16:07:45 +0900

    만료된 코드 아이템까지 복구

--- src/main/java/app/service/CodeService.java
+++ src/main/java/app/service/CodeService.java
@@ -1,11 +1,14 @@
 package app.service;
 
 import app.domain.Code;
+import app.domain.Item;
 import app.repository.CodeRepository;
+import app.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +16,9 @@ import java.util.Optional;
 public class CodeService {
     @Autowired
     private CodeRepository codeRepository;
+    @Autowired
+    private ItemRepository itemRepository;
+
     public List<Code> getAllCodes() {
         return codeRepository.findAll();
     }
@@ -35,7 +41,10 @@ public class CodeService {
         List<Code> codes = codeRepository.findAll();
         for (Code code : codes) {
             // 오늘 날짜 기준으로 30일 이전 코드 삭제
-            if (code.getTime().plusDays(ExpiredDays).isBefore(code.getTime())) {
+            if (code.getTime().isBefore(LocalDateTime.now().minusDays(ExpiredDays))) {
+                // item 복구
+                Item item = itemRepository.findByItemCode(code.getItemCode());
+                item.setQuantity(item.getQuantity() + code.getQuantity());
                 codeRepository.delete(code);
             }
         }
---
+++ src/test/java/app/service/CodeServiceTest.java
@@ -0,0 +1,54 @@
+package app.service;
+
+import app.domain.Code;
+import app.domain.Item;
+import app.repository.CodeRepository;
+import app.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class CodeServiceTest {
+    @Autowired
+    private CodeService codeService;
+    @Autowired
+    private CodeRepository codeRepository;
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @DisplayName("기간이 지난 코드 삭제 테스트")
+    @Test
+    public void deleteExpiredCodesTest() {
+        // given
+        LocalDateTime oldDate = LocalDateTime.now().minusDays(31);
+        codeRepository.save(new Code("code1", oldDate, 1, 1));
+        codeRepository.save(new Code("code2", LocalDateTime.now(), 2, 2));
+        // when
+        codeService.deleteExpiredCodes();
+        // then
+        Code code1 = codeRepository.findByCode("code1");
+        Code code2 = codeRepository.findByCode("code2");
+        assert code1 == null : "기간이 지난 코드가 삭제되지 않았습니다." + code1;
+        assert code2 != null : "기간이 지나지 않은 코드가 삭제되었습니다." + null;
+    }
+    @DisplayName("기간이 지난 코드 삭제되고 item 복구 테스트")
+    @Test
+    public void deleteExpiredCodesAndRecoverItemTest() {
+        // given
+        LocalDateTime oldDate = LocalDateTime.now().minusDays(31);
+        codeRepository.save(new Code("code1", oldDate, 999, 1));
+        itemRepository.save(new Item("item1", 10, 999));
+        // when
+        codeService.deleteExpiredCodes();
+        // then
+        Code code1 = codeRepository.findByCode("code1");
+        Item item1 = itemRepository.findByItemCode(999);
+        assert code1 == null : "기간이 지난 코드가 삭제되지 않았습니다." + code1;
+        assert item1.getQuantity() == 11 : "기간이 지난 코드의 수량이 item에 복구되지 않았습니다." + item1.getQuantity();
+    }
+}
