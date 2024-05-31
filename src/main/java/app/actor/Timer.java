d2b962f3967e2d84dc2564128efe50b75fd55656
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-05-31 13:57:02 +0900

    1시간 마다 만료된 코드 삭제

--- src/main/java/app/actor/Timer.java
+++ src/main/java/app/actor/Timer.java
@@ -1,14 +1,19 @@
 package app.actor;
 
+import app.service.CodeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class Timer {
-    // 고정된 주기로 실행 (밀리초 단위)
-    @Scheduled(fixedRate = 1000)
+    @Autowired
+    CodeService codeService;
+    // 고정된 시간에 실행 (cron 표현식) - 1시간 마다 실행
+    @Scheduled(cron = "0 0 0/1 * * *")
     public void fixedRateTask() {
-        log.info("Fixed rate task - " + System.currentTimeMillis() / 1000);
+        log.info("deleteExpiredCodes");
+        codeService.deleteExpiredCodes();
     }
 }
