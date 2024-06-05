e0e31668aecdd524a16dd2613cc8239fa671a0d3
jackpod7002 <jackpod7002@konkuk.ac.kr>
2024-06-05 16:55:27 +0900

    no message

--- src/main/resources/static/javascript/PrepaymentResult.js
+++ src/main/resources/static/javascript/PrepaymentResult.js
@@ -1,3 +1,7 @@
+
+var OrderResponse  = JSON.parse(localStorage.getItem('OrderResponse'));
+
 $(document).ready(function(){
-    
+    $(".code").text(OrderResponse.code.code);
+    $(".coordinates").text("("+OrderResponse.info.x + ", " + OrderResponse.info.y+")");
 });
