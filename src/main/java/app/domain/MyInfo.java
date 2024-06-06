8cab2b441d7c09a3a661841aecdf8ef42140fe21
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-06-06 20:04:26 +0900

    @Getter

--- src/main/java/app/domain/MyInfo.java
+++ src/main/java/app/domain/MyInfo.java
@@ -3,17 +3,25 @@ package app.domain;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-@Getter
+
 @Component
 public class MyInfo {
     @Value("${MY_IP:127.0.0.1}")
     private String ip;
+
+    @Getter
     @Value("${MY_PORT:11120}")
     private int port;
+
+    @Getter
     @Value("${MY_ID:team5}")
     private String id;
+
+    @Getter
     @Value("${MY_X:15}")
     private int x;
+
+    @Getter
     @Value("${MY_Y:20}")
     private int y;
 
